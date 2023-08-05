2d8fa12078f3cd37861f75a545fc8b396861c84a
Arun Chandra <arunchandra19082004@gmail.com>
2023-08-05 20:03:19 +0530

    Login and Announcement Page done
    Both the frontend and backend done.

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import mysql.connector
-from flask import Flask, request, jsonify, session, render_template, url_for
-import datetime
+from flask import Flask, request, jsonify, session, render_template, url_for, redirect, flash
+from datetime import datetime
 
 app = Flask(__name__)
 app.secret_key = 'LAXARUN'
@@ -8,16 +8,6 @@ app.secret_key = 'LAXARUN'
 @app.route('/')
 def home():
     return render_template('homepage.html')
-
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    if request.method == "POST":
-        request.form.email 
-    return render_template('login.html')
-
-@app.route('/announcement_board')
-def announcement():
-    return render_template('announcement.html')
 
 @app.route('/resources')
 def resources():
@@ -27,19 +17,69 @@ def resources():
 def faculty():
     return render_template('faculty.html')
 
+@app.route('/logout')
+def logout():
+    session.clear()
+    return redirect('/')
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if 'email' in session and 'actor' in session:
+        return redirect('/')
+    
+    if request.method == "POST":
+        email = request.form['email']
+        password = request.form['password']
+        role = request.form.get('role')
+
+        # Assuming you have a database connection
+        logindb = mysql.connector.connect(
+            host='localhost',
+            user='root',
+            password='',
+            database='sf_logininfo'
+        )
+        cursor = logindb.cursor()
+
+        query = "SELECT email, actor FROM main WHERE email = %s AND password = %s AND actor = %s"
+        cursor.execute(query, (email, password, role))
+        result = cursor.fetchone()
+
+        cursor.close()
+        logindb.close()
+
+        if result:
+            session['email'] = email
+            session['actor'] = role
+            if request.headers.get('X-Requested-With') == 'XMLHttpRequest':
+                return jsonify({'success': True})
+            else:
+                return redirect('/')  # Redirect to homepage after successful login
+        else:
+            error_message = "Invalid credentials or role selection. Please try again."
+            flash(error_message, 'error')  # Flash error message
+
+            if request.headers.get('X-Requested-With') == 'XMLHttpRequest':
+                return jsonify({'success': False, 'error': error_message})
+            else:
+                return render_template('login.html')
+
+    return render_template('login.html')
+
 
 def get_faculty_name(email):
 
     facultydb = mysql.connector.connect(
         host='localhost',
-        user='your_db_username',
-        password='your_db_password',
-        database='your_db_name'
+        user='root',
+        password='',
+        database='sf_facultyinfo'
     )
 
     cursor = facultydb.cursor()
 
-    query = "SELECT name FROM main WHERE email = %s"
+    query = "SELECT Name FROM main WHERE email = %s"
     cursor.execute(query, (email,))
 
     result = cursor.fetchone()
@@ -52,20 +92,56 @@ def get_faculty_name(email):
     else:
         return None  
 
-@app.route('/getannouncement')
-def get_announcement():
+
+@app.route('/create_announcement', methods=['POST'])
+def create_announcement():
+    if 'email' in session and 'actor' in session and session['actor'] == 'Faculty':
+        # Get the content from the form submission
+        content = request.form.get('content')
+        email = session['email']
+        # actor = session['actor']
+        name = get_faculty_name(email)
+        current_datetime = datetime.now()
+
+        # Format the date and time to match MySQL's DATE and TIME formats
+        mysql_date_format = current_datetime.strftime('%Y-%m-%d')
+        mysql_time_format = current_datetime.strftime('%H:%M:%S')
+
+        announcementdb = mysql.connector.connect(
+                host='localhost',
+                user='root',
+                password='',
+                database='sf_announcement'
+        )
+        cursor = announcementdb.cursor()
+
+        insert_query = "INSERT INTO main (Name, Announcement, Date, Time) VALUES (%s, %s, %s, %s)"
+        values = (name, content, mysql_date_format, mysql_time_format)
+        cursor.execute(insert_query, values)
+        announcementdb.commit()
+
+        cursor.close()
+        announcementdb.close()
+
+        return redirect(url_for('announcements'))
+    else:
+        return redirect('/')
+    
+@app.route('/announcements', methods=['GET', 'POST'])
+def announcements():
+    if 'email' not in session or 'actor' not in session:
+        return redirect('/')
+    
     try:
-        # Assuming you have a database connection
         announcementdb = mysql.connector.connect(
             host='localhost',
             user='root',
             password='',
             database='sf_announcement'
         )
+        
         cursor = announcementdb.cursor()
-
-        # Retrieve announcements from the database
-        select_query = "SELECT id, Name, announcement, Date, Time FROM announcements ORDER BY Date DESC, Time DESC"
+        select_query = "SELECT Id, Name, Announcement, Date, Time FROM main ORDER BY Date DESC, Time DESC"
         cursor.execute(select_query)
         result = cursor.fetchall()
 
@@ -74,92 +150,51 @@ def get_announcement():
             id, name, content, date, time = row
             announcement = {
                 'id': id,
-                'actor': 'faculty' if name == get_faculty_name(session['email']) else 'student',
                 'name': name,
                 'content': content,
-                'date': date.strftime('%B %d, %Y'),
-                'time': time.strftime('%I:%M %p')
+                'date': date,
+                'time': time
             }
             announcements.append(announcement)
-
-        return jsonify(announcements=announcements)
+        
+        return render_template('announcement.html', announcements=announcements, get_faculty_name = get_faculty_name)
     except Exception as e:
-        return jsonify(success=False, error=str(e))
+        # Handle any errors here
+        return render_template('error.html', error_message=str(e))
+    finally:
+        if announcementdb.is_connected():
+            cursor.close()
+            announcementdb.close()
 
-@app.route('/postannouncement', methods=['POST'])
-def post_announcement():
-    if 'email' in session and 'actor' in session:
-        email = session['email']
-        actor = session['actor']
-        name = get_faculty_name(email)
-
-        # Get data from the request
-        content = request.json.get('content')
-        date = request.json.get('date')
-        time = request.json.get('time')
-
+@app.route('/delete_announcement/<int:announcement_id>', methods=['GET', 'POST'])
+def delete_announcement(announcement_id):
+    if 'email' in session and 'actor' in session and session['actor'] == 'Faculty':
         try:
-            # Assuming you have a database connection
+            # Connect to the database
             announcementdb = mysql.connector.connect(
-                host='localhost',
-                user='root',
-                password='',
-                database='sf_announcement'
+            host='localhost',
+            user='root',
+            password='',
+            database='sf_announcement'
             )
+        
             cursor = announcementdb.cursor()
 
-            # Insert the announcement into the database
-            insert_query = "INSERT INTO announcements (Name, announcement, Date, Time) VALUES (%s, %s, %s, %s)"
-            values = (name, content, date, time)
-            cursor.execute(insert_query, values)
+            # Delete the announcement from the database
+            delete_query = "DELETE FROM main WHERE Id = %s"
+            cursor.execute(delete_query, (announcement_id,))
             announcementdb.commit()
 
-            return jsonify({'success': True})
+            return "Success", 200
         except Exception as e:
-            return jsonify({'success': False, 'error': str(e)})
+            print(e)
+            return "Error", 500  # Return an error response
         finally:
             cursor.close()
             announcementdb.close()
     else:
-        return jsonify({'success': False, 'error': 'Unauthorized access.'})
-
-@app.route('/deleteannouncement', methods=['POST'])
-def delete_announcement():
-    if 'email' in session and 'actor' in session and session['actor'] == 'faculty':
-        email = session['email']
-        actor = session['actor']
-        name = get_faculty_name(email)
-
-        announcement_id = request.json.get('announcement_id')
-        announcement_name = request.json.get('announcement_name')
-
-        if name == announcement_name:
-            try:
-                # Assuming you have a database connection
-                announcementdb = mysql.connector.connect(
-                    host='localhost',
-                    user='root',
-                    password='',
-                    database='sf_announcement'
-                )
-                cursor = announcementdb.cursor()
-
-                # Delete the announcement from the database
-                delete_query = "DELETE FROM announcements WHERE id = %s"
-                cursor.execute(delete_query, (announcement_id,))
-                announcementdb.commit()
-
-                return jsonify({'success': True})
-            except Exception as e:
-                return jsonify({'success': False, 'error': str(e)})
-            finally:
-                cursor.close()
-                announcementdb.close()
-        else:
-            return jsonify({'success': False, 'error': 'You can only delete your own announcements.'})
-    else:
-        return jsonify({'success': False, 'error': 'Unauthorized access.'})
-
+        return "Unauthorized", 403
+    
 
 
 if __name__ == '__main__':
